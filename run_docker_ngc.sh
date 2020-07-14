2996897d0ce6a88d17363676eb426d8d0f859336
kmjeon <kmjeon@int-flow.com>
2020-07-15 01:34:35 +0900

    installing pytorch and mxnet

--- run_docker_ngc.sh
+++ run_docker_ngc.sh
@@ -8,14 +8,14 @@
 mkdir ~intflow/works
 
 #Run Dockers for voice conversion (tf1 + pytorch)
-sudo docker run --name l4t_deepstream \
+sudo docker run --name l4t_deepstream_BT1 \
 --net=host \
 --runtime nvidia  \
 -w /opt/nvidia/deepstream/deepstream-5.0 \
 --mount type=bind,src=/home/intflow/works,dst=/works \
 --privileged \
 --ipc=host \
--it --rm intflow/deepstream:xnx_lvl1
+-it --rm intflow/deepstream:xnx_BT1
 
 #--env="DISPLAY" --volume="$HOME/.Xauthority:/root/.Xauthority:rw" \
 #-v /tmp/.X11-unix/:/tmp/.X11-unix \
--- update_docker_ngc.sh
+++ update_docker_ngc.sh
@@ -1,8 +1,8 @@
 
 #!/bin/bash
 
-sudo docker commit l4t_deepstream deepstream:xnx
+sudo docker commit l4t_deepstream_BT1 deepstream:xnx_BT1
 sudo docker login docker.io -u kmjeon -p 1011910119a!
-sudo docker tag deepstream:xnx intflow/deepstream:xnx
-sudo docker push intflow/deepstream:xnx_lvl1
+sudo docker tag deepstream:xnx_BT1 intflow/deepstream:xnx_BT1
+sudo docker push intflow/deepstream:xnx_BT1
 
